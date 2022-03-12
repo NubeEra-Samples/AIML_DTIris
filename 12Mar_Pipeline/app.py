dc8c8b375c16b3cc32fd8d5ec782c72c633b713d
mujahed85 <mujahed.mca@gmail.com>
2022-03-12 16:15:30 +0530

    updated app.py and requirements.txt

---
+++ 12Mar_Pipeline/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<b>Welcome Python From Flask with Docker</b>"
+
+if __name__=="__main__":
+    app.run(debug=True,host="0.0.0.0",port=5000)

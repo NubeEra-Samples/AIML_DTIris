49ef860bd047e722b7bc3971eddc0e0885a58cad
mujahed85 <mujahed.mca@gmail.com>
2022-03-12 16:26:27 +0530

    Dockerfile updated

--- 12Mar_Pipeline/app.py
+++ 12Mar_Pipeline/app.py
@@ -7,4 +7,5 @@ def index():
     return "<b>Welcome Python From Flask with Docker</b>"
 
 if __name__=="__main__":
-    app.run(debug=True,host="0.0.0.0",port=5000)
+    app.run(debug=True)
+    #app.run(debug=True,host="0.0.0.0",port=5000)
